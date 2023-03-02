c3dae1b7c58782eedcbe9547e3e54928f2735e70
Chris Platt <chris.platt@insiteone.com>
2023-03-02 04:33:26 +0000

    adding a little interactivity

--- buildOhif.sh
+++ buildOhif.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
+
+echo "Caching sudo - we will need it later"
+sudo -v
+
 SSL_CERT=$HOME/ssl/tiny.insiteone.com.pem
 SSL_KEY=$HOME/ssl/tiny.insiteone.com.key
 
 BASE_WORKDIR=$HOME/docker
 OHIFDIR=$BASE_WORKDIR/Viewers
 
+echo "Setting up directories"
 # set up directories 
-[ -d $BASE_WORKDIR ] || mkdir $BASE_WORKDIR
-[ -d $OHIFDIR ] || mkdir $OHIFDIR
+[ -d $BASE_WORKDIR ] || mkdir -v $BASE_WORKDIR
+[ -d $OHIFDIR ] || mkdir -v $OHIFDIR
 
+echo "Cloning https://github.com/OHIF/Viewers.git"
 # clone the v3-stable repository
 git clone -b v3-stable https://github.com/OHIF/Viewers.git $OHIFDIR
 
+echo "Setting up certificates"
 # put certs in place
-[ -d $OHIFDIR/.docker/ssl ] || mkdir $OHIFDIR/.docker/ssl
+[ -d $OHIFDIR/.docker/ssl ] || mkdir -v $OHIFDIR/.docker/ssl
+cp -v $SSL_CERT $OHIFDIR/.docker/ssl
+cp -v $SSL_KEY $OHIFDIR/.docker/ssl
 
+echo "Pulling OHIF and nginx configurations from github"
 # grab configuration files from uca git repository
 curl $OHIFDIR/.docker/ohif-v3-default.js https://raw.githubusercontent.com/cplatt-iso/OHIF/main/Config/ohif-v3-default.js > $OHIFDIR/.docker/ohif-v3-default.js
 curl $OHIFDIR/.docker/nginx-default.conf https://raw.githubusercontent.com/cplatt-iso/OHIF/main/Config/nginx-default.conf > $OHIFDIR/.docker/nginx-default.conf
-cp $SSL_CERT $OHIFDIR/.docker/ssl
-cp $SSL_KEY $OHIFDIR/.docker/ssl
 
+echo "Modifying Dockerfile to incorporate our configuration"
 # Modify Dockerfile 
 sed -i '/USER nginx/a \
 COPY --chown=nginx:nginx .docker/nginx-default.conf /etc/nginx/conf.d/default-ssl.conf \
@@ -33,5 +42,6 @@ RUN rm /usr/share/nginx/html/app-config.js || true \
 COPY .docker/ohif-v3-default.js /usr/share/nginx/html/app-config.js' $OHIFDIR/Dockerfile
 
 cd $OHIFDIR
+echo "Building docker image"
 sudo docker build -t ohif/viewer:uca .
 
